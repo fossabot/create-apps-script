8b45e713e4b83aeb8570ee84c11a4c0b6fd87108
CSmith <CSmith14@pm.me>
2019-06-11 00:52:56 -0500

    Tests and configuration

---
+++ test/Config.methods.spec.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const homedir = require('os').homedir()
+
+const Config = require('../lib/classes/Config')
+
+const config = new Config('eslintrc')
+const testp = path.resolve(__dirname, '../Readme.md')
+
+/* eslint-disable no-undef */
+
+describe('Class Methods', () => {
+  describe('import', () => {
+    describe('Input validation', () => {
+      it('Should reject invalid paths', (done) => { // eslint-disable-line consistent-return
+        const testpath = path.join(homedir, 'some', 'ridiculous', 'path', 'which', 'is-nonexistent', 'supercalifragilistic.expialidocious.json')
+
+        try {
+          config.import({ 'filepath': testpath })
+        } catch (err) {
+          return done()
+        }
+
+        done(Error(`Method accepted provided path: ${testpath}`))
+      }) // End Should reject bad paths
+
+      it('Should reject unsupported extensions', (done) => { // eslint-disable-line consistent-return
+        const testpath = path.resolve(__dirname, '../Readme.md')
+
+        try {
+          config.import({ 'filepath': testpath })
+        } catch (err) {
+          return done()
+        }
+
+        done(Error(`Method accepted an unsupported file type: ${testpath}`))
+      }) // End Should reject unsupported extensions
+    }) /* Input validation */
+
+    it('Should fail', (done) => { // eslint-disable-line consistent-return
+      try {
+        const obj = config.importFile(testp)
+        console.log(obj)
+      } catch (e) {
+        console.log(e)
+        done()
+      }
+
+      done(Error())
+    }) // End Should fail
+  }) /* importFile */
+}) /* Class Methods */
---
+++ test/resources/.eslintrc.test.js
@@ -0,0 +1,62 @@
+const Config = require('../../lib/classes/Config')
+const {
+  print
+} = require( 'q-i' )
+
+const enumerable = true
+const configurable = true
+const value = true
+const globalsPropDescriptor = () => {
+  const desc = Object.create( {} )
+  Object.assign( desc, {
+    enumerable,
+    configurable,
+    value
+  } )
+  return desc
+}
+
+const reduceFn = ( prev, curr ) => {
+  return Object.assign( prev, {
+    [ curr ]: globalsPropDescriptor()
+  } )
+}
+
+const globalSymbols = [
+  'Calendar', 'CalendarApp', 'CardService', 'Charts', 'ContactsApp', 'DataStudioApp', 'DocumentApp', 'Drive', 'DriveApp', 'FirebaseApp', 'FormApp', 'Gmail', 'GmailApp', 'GroupsApp', 'HtmlService', 'LanguageApp', 'MailApp', 'Maps', 'OAuth1', 'OAuth2', 'PropertiesService', 'SitesApp', 'Slides', 'SlidesApp', 'SpreadsheetApp'
+]
+
+const globals = Object.defineProperties(
+  Object.create( null ),
+  Object.assign( {},
+    globalSymbols.reduce( ( p, c ) => reduceFn( p, c ),
+      Object.create( {} ) /* <== Initial value */
+    )
+  )
+)
+
+const env = Object.freeze( {
+  'amd': true,
+  'googleappsscript/googleappsscript': true,
+  'node': true
+} )
+
+const parser = 'babel-eslint'
+
+const parserOptions = Object.freeze( {
+  'sourceType': 'module'
+} )
+
+const plugins = [].push( 'googleappsscript' )
+
+const root = true
+
+const config = {
+  'extends': '',
+  'root': true
+};
+
+// globals.
+
+// ANCHOR module.exports
+module.exports = Object.create( config );
