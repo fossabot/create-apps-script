e932d7dc432f175f46f94024215be25cf875da9a
CSmith <CSmith14@pm.me>
2019-06-12 13:04:08 -0500

    style: Remove eslint, add XO
    Attempt to simplify styling and linting. Removed eslint and series of related dev dependencies,
    added XO.

--- test/Config.spec.js
+++ test/Config.spec.js
@@ -1,10 +1,13 @@
 /* eslint-env mocha */
-const assert = require('assert');
-const chalk = require('chalk');
-const Config = require('../src/lib/classes/Config');
+/* eslint-disable max-nested-callbacks */
+
+const assert = require( 'assert' );
+const Config = require( '../src/lib/classes/Config' );
 
 function testSuite() {
-	return describe('Config Class', () => {
+
+	return describe( 'Config Class', () => {
+
 		const testArgument = {
 			dir: '.',
 			dot: false,
@@ -13,45 +16,55 @@ function testSuite() {
 		};
 
 		/** @type {Config} The master Config instance   */
-		const config = new Config(testArgument);
+		const config = new Config( testArgument );
 
-		describe('Config Class', () => {
-			describe('Constructor', () => {
-				describe('Default values', () => {
-					/* eslint-disable camelcase */
+		describe( 'Config Class', () => {
+
+			describe( 'Constructor', () => {
+
+				describe( 'Default values', () => {
+
 					/** @type {Config} The instance to test default value assignment */
-					const defaultInstance = new Config({});
-					const defaultInstance_keys = Object.keys(defaultInstance);
-					/* eslint-enable camelcase */
+					const defaultInstance = new Config( {} );
+					const defaultInstanceKeys = Object.keys( defaultInstance );
 
-					it('Should have prop-values identical to defaults', done => { // eslint-disable-line cosistent-return
-						const nonMatches = defaultInstance_keys.map(key => {
-							return ((String(ConfigDefault[key]) === String(defaultInstance[key])));
-						}).map((v, i) => {
+					it( 'Should have prop-values identical to defaults', done => {
+						const nonMatches = defaultInstanceKeys.map( key => {
+							return ( ( String( Config[key] ) === String( defaultInstance[key] ) ) );
+						} ).map( ( v, i ) => {
+
 							/* Replace any non-matches with the key label */
-							return !v ? defaultInstance_keys[i] : null;
-						}).filter(v => v); /* Then simply remove null values */
+							return v ? null : defaultInstanceKeys[i];
 
-						if (nonMatches.length > 0) {
-							return done(new Error(`${nonMatches.length} property values are not identical to expected.`));
+						} ).filter( v => v ); /* Then simply remove null values */
+						if ( nonMatches.length > 0 ) {
+							return done( new Error( `${nonMatches.length} property values are not identical to expected.` ) );
 						}
 
 						done();
-					}); // End-Test: Should have prop-values identical to defaults |>
-				}); // End-Suite: Default values ||
+					} ); // End-Test: Should have prop-values identical to defaults |>
+				} ); // End-Suite: Default values ||
 
-				describe('Schema Resolution', () => {
-					it('Should load the object into own property "schema"', done => { // eslint-disable-line consistent-return
-						if (!config.schema) {
-							return done(new Error('No schema property'));
+				describe( 'Schema Resolution', () => {
+
+					it( 'Should load the object into own property "schema"', done => {
+
+						if ( !config.schema ) {
+							return done( new Error( 'No schema property' ) );
 						}
 
 						done();
-					}); // End-Test: Should load the object into own property "schema" |>
-				}); // End-Suite: Schema Resolution ||
-			}); // End-Suite: Constructor ||
-		}); // End-Suite: Config Files Base Object Props & Methods ||
-	}); // End-Suite: Config Class ||
+
+					} ); // End-Test: Should load the object into own property "schema" |>
+
+				} ); // End-Suite: Schema Resolution ||
+
+			} ); // End-Suite: Constructor ||
+
+		} ); // End-Suite: Config Files Base Object Props & Methods ||
+
+	} ); // End-Suite: Config Class ||
+
 }
 
 module.exports = /* >> | << */ testSuite;
